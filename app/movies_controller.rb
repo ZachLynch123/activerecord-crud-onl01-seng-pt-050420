a2426fbeed52fb0370f023bfa2aba2fda6491061
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-19 19:55:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,8 +71,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   Move.update(title: "Even Awesomer Flick")
   Movie.save
-  __
-  __
+  Movie
 end
 
 def can_update_using_update_method
